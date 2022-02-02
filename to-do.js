acfe375dfa74af271fb59573559275460e1d1be9
duvancho303 <duvancho303@gmail.com>
2022-02-02 16:21:06 -0700

    adding more js

--- to-do.js
+++ to-do.js
@@ -1,6 +1,7 @@
 let addToDoButton = document.getElementById('add');
 let ToDoContainer = document.getElementById('ToDoContainer');
-let input = document.getElementById('input', function(){
-    var paragraph = document.createElement('p')
-    ToDoContainer
-});
+let input = document.getElementById('input');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')         // here every time we click on "add" button, it will create a new paragraph
+})
