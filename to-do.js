4f32f13cf9c48ab0f6215ae2e9e630b8b3397317
duvancho303 <duvancho303@gmail.com>
2022-02-02 16:03:35 -0700

    adding more js

--- to-do.js
+++ to-do.js
@@ -1,1 +1,6 @@
 let addToDoButton = document.getElementById('add');
+let ToDoContainer = document.getElementById('ToDoContainer');
+let input = document.getElementById('input', function(){
+    var paragraph = document.createElement('p')
+    ToDoContainer
+});
