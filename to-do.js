373450fc54882844b617be7d10b30d09a22a57c1
duvancho303 <duvancho303@gmail.com>
2022-02-02 17:03:56 -0700

    aading more js

--- to-do.js
+++ to-do.js
@@ -4,6 +4,14 @@ let input = document.getElementById('input');
 
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p')         // here every time we click on "add" button, it will create a new paragraph
+    paragraph.classList.add('paragraph-styling');
     paragraph.innerText = input.value;
-    toDoContainer.appendChild(paragraph);
+    toDoContainer.appendChild(paragraph);               //will create the "P" paragraph inside the  container 
+    input.value = "";                                   //empty input box after i type something
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
 })
